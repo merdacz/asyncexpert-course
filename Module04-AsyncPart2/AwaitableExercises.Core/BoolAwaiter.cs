2f50c42fdd6169962aa49e9d36757ea7ecea2a2e
Marcin Daczkowski <merdacz@gmail.com>
2020-08-02 23:12:29 +0200

    week #4 homework - awaitables

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,13 +5,31 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool @this)
+        {
+            return new BoolAwaiter(@this);
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool value;
+
+        public BoolAwaiter(bool value)
+        {
+            this.value = value;
+        }
+
+        public bool IsCompleted => false;
+
+        public bool GetResult()
+        {
+            return this.value;
+        }
+
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            continuation();
         }
     }
 }
--- Module04-AsyncPart2/AwaitableExercises.Tests/BoolAwaiterTests.cs
+++ Module04-AsyncPart2/AwaitableExercises.Tests/BoolAwaiterTests.cs
@@ -5,51 +5,49 @@ using AwaitableExercises.Core;
 
 namespace AwaitableExercises.Tests
 {
-    // UNCOMMENT all asserts when start to implement this exercise!
-    // It is by default commented to make compilation possible for other lessons.
     public class BoolAwaiterTests
     {
         [Fact]
         public async Task AwaitFalse()
         {
-            //Assert.False(await false);
+            Assert.False(await false);
         }
 
         [Fact]
         public async Task AwaitTrue()
         {
-            //Assert.True(await true);
+            Assert.True(await true);
         }
 
         [Fact]
         
         public async Task AwaitAwaitFalse()
         {
-            //Assert.False(await await false);
+            Assert.False(await await false);
         }
 
         [Fact]
         public async Task AwaitAwaitTrue()
         {
-            //Assert.True(await await true);
+            Assert.True(await await true);
         }
 
         [Fact]
         public async Task AwaitOperatorsTrueAndTrue()
         {
-            //Assert.True(await true && await true);
+            Assert.True(await true && await true);
         }
 
         [Fact]
         public async Task AwaitOperatorsTrueAndFalse()
         {
-            //Assert.False(await true && await false);
+            Assert.False(await true && await false);
         }
 
         [Fact]
         public async Task AwaitOperatorsFalseAndTrue()
         {
-            //Assert.False(await false && await true);
+            Assert.False(await false && await true);
         }
     }
 }

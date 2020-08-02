8c1934b21a093eb4482f96cb2d5b26c2c69b77e3
Marcin Daczkowski <merdacz@gmail.com>
2020-08-02 23:05:54 +0200

    week #4 homework - tcs

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,7 +8,33 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var tcs = new TaskCompletionSource<string>();
+            var process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo = new ProcessStartInfo(path, args)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+            process.Exited += (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                if (senderProcess?.ExitCode != 0)
+                {
+                    var error = senderProcess?.StandardError.ReadToEnd();
+                    tcs.SetException(new Exception(error));
+                }
+                else
+                {
+                    var output = senderProcess?.StandardOutput.ReadToEnd();
+                    tcs.SetResult(output);
+                }
+                
+                senderProcess?.Dispose();
+            };
+            process.Start();
+
+            return tcs.Task;
         }
     }
 }
